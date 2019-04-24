8058f2af0ce0046aa5e44d9f169810de9ff2d7e9
Paul Bailey <paul.m.bailey@gmail.com>
2019-04-24 17:17:39 -0500

    Update urls.py

--- flowjs/urls.py
+++ flowjs/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import patterns, url
+from django.urls import path, re_path, include
 from django.views.decorators.csrf import csrf_exempt
 
 from views import UploadView, CheckStateView
 
+app_name = 'flowjs'
+
 # JSON REQUESTS
-urlpatterns = patterns('',
-    url(r'^upload/$', csrf_exempt(UploadView.as_view())),
-    url(r'^state/$', csrf_exempt(CheckStateView.as_view())),
-)
+urlpatterns = [
+    re_path(r'^upload/$', csrf_exempt(UploadView.as_view())),
+    re_path(r'^state/$', csrf_exempt(CheckStateView.as_view())),
+]
